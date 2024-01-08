a5b7259c3033f021686f42472a38458095319871
Lhsales <48722995+Lhsales@users.noreply.github.com>
2024-01-07 22:34:18 -0300

    Criação layout footer base
    - Criado layout base para footer base.

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -7,13 +7,18 @@
     <title>Laravel | @yield('head-title')</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+
 </head>
 <body>
-    @include('layout.include.top')
+    @include('layout.include.header')
 
 
     
-    @include('layout.include.bottom')
+    @include('layout.include.footer')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ resources/views/layout/include/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer-basic">
+    <footer>
+        <div class="social">
+            <a href="#"><i class="icon ion-social-instagram"></i></a>
+            <a href="#"><i class="icon ion-social-twitter"></i></a>
+            <a href="#"><i class="icon ion-social-facebook"></i></a>
+        </div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="{{ route('home') }}">Home</a></li>
+            <li class="list-inline-item"><a href="{{ route('experiences.index') }}">Experiências</a></li>
+            <li class="list-inline-item"><a href="{{ route('knowledges.index') }}">Conhecimentos</a></li>
+        </ul>
+        <p class="copyright">Luã Henrique Sales © 2024</p>
+    </footer>
+</div>
