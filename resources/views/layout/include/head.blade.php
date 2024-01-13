d69aafcd71846d4c855e04c981f1521d5d0d2f50
Lhsales <48722995+Lhsales@users.noreply.github.com>
2024-01-13 20:58:12 -0300

    Inclusão de plugin para Select Range
    - Adicionado plugin visual de select range para nível de conhecimento.

--- resources/views/layout/include/head.blade.php
+++ resources/views/layout/include/head.blade.php
@@ -1,2 +1,4 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />
+<link href="{{ URL::asset('css/horizontal_selector.css') }}" rel="stylesheet">
